3533cb315d1e1dda6b0cb1988412d6ae35613d04
Groupystinks <jason0911520@gmail.com>
2015-12-16 10:59:20 +0800

    added new container Thread

--- src/containers/Group/Group.js
+++ src/containers/Group/Group.js
@@ -1,18 +1,28 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {pushState} from 'redux-router';
+import {Thread} from 'containers';
 import {Scroller, BlockList} from 'components';
 
 const styles = require('./Group.scss');
 
 @connect(
-  state => ({groupData: state.word.groupData})
+  state => ({groupData: state.word.groupData}),
+  dispatch => bindActionCreators({
+    pushState
+  }, dispatch)
 )
 export default class Group extends Component {
   static propTypes = {
+    children: PropTypes.object,
     groupData: PropTypes.array,
+    pushState: PropTypes.func
   }
 
-  _onGroupSelected = () => {};
+  _onGroupSelected = (group) => {
+    this.props.pushState(null, `/thread/${group.name}`);
+  };
 
   render() {
     const {groupData} = this.props;
@@ -27,6 +37,9 @@ export default class Group extends Component {
           />
          </Scroller>
        ) : null}
+         <div className={styles.thread}>
+           <Thread/>
+         </div>
        </div>
     );
   }
