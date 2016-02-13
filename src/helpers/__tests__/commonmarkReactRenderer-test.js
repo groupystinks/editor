f4f18e02f1930a8e6cf48464dc1636ff68bb7a12
Groupystinks <jason0911520@gmail.com>
2016-02-13 14:43:09 +0800

    upated react renderer test script

--- src/helpers/__tests__/commonmarkReactRenderer-test.js
+++ src/helpers/__tests__/commonmarkReactRenderer-test.js
@@ -3,17 +3,14 @@ import CommonMark from 'commonmark';
 import ReactRenderer from '../commonmarkReactRenderer';
 
 describe('commonmarkReactRenderer', () => {
-  const blocksCount = 3;
-  const input = '# This is a header\n\n- And this is **a _amazing_** word\n\n```function()```';
-
   const parser = new CommonMark.Parser();
   const renderer = new ReactRenderer();
-
-  const ast = parser.parse(input);
-  // // Result of this operation will be an array of React elements
-  const reactComponents = renderer.render(ast);
-
   it('should render array of React elements', () => {
+    const blocksCount = 3;
+    const input = '## This is a header\n\n- And this is **a _amazing_** word\n\n```function()```';
+
+    const ast = parser.parse(input);
+    const reactComponents = renderer.render(ast);
     expect(reactComponents.length).to.equal(blocksCount);
   });
 });
